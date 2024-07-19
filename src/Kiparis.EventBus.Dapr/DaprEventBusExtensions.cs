a3296861d17bb74328cb364481f1707b87a917c0
Alexander Bocharov <alex.bocharov@outlook.com>
2024-07-19 15:08:17 +0300

    Add Dapr EventBus support
    - Removed `Aspire.Hosting.AppHost` v8.0.2 from Directory.Packages.props.
    - Added `Dapr.Client` v1.13.1 and `Microsoft.Extensions.Hosting.Abstractions` v8.0.0 to Directory.Packages.props.
    - Updated `Kiparis.sln` with new project GUIDs, added `src` solution folder, and included new projects `Kiparis.EventBus` and `Kiparis.EventBus.Dapr`.
    - Organized projects under `src` folder in `Kiparis.sln` and updated `ExtensibilityGlobals`.
    - Added `DaprEventBus`, `DaprEventBusConstants`, and `DaprEventBusExtensions` classes for Dapr event publishing.
    - Created `Kiparis.EventBus.Dapr.csproj` targeting .NET 8.0 with necessary package and project references.
    - Introduced `IEventBus` interface, `IIntegrationEventHandler` interfaces, `IntegrationEvent` record, and `GenericTypeExtensions` class.
    - Established `Kiparis.EventBus.csproj` targeting .NET 8.0.

---
+++ src/Kiparis.EventBus.Dapr/DaprEventBusExtensions.cs
@@ -0,0 +1,43 @@
+﻿// SPDX-License-Identifier: BUSL-1.1
+//
+// Copyright (c) 2024 Parusnik-Belgorod, LLC. All rights reserved.
+// Use of this software is govered by the Business Source License included
+// in the LICENSE file of this repository and at www.mariadb.com/bsl11.
+//
+// ANY USE OF THE LICENSED WORK IN VIOLATION OF THIS LICENSE WILL AUTOMATICALLY
+// TERMINATE YOUR RIGHTS UNDER THIS LICENSE FOR THE CURRENT AND ALL OTHER
+// VERSIONS OF THE LICENSED WORK.
+//
+// THIS LICENSE DOES NOT GRANT YOU ANY RIGHT IN ANY TRADEMARK OR LOGO OF
+// LICENSOR OR ITS AFFILIATES (PROVIDED THAT YOU MAY USE A TRADEMARK OR LOGO OF
+// LICENSOR AS EXPRESSLY REQUIRED BY THIS LICENSE).
+//
+// TO THE EXTENT PERMITTED BY APPLICABLE LAW, THE LICENSED WORK IS PROVIDED ON
+// AN “AS IS” BASIS. LICENSOR HEREBY DISCLAIMS ALL WARRANTIES AND CONDITIONS,
+// EXPRESS OR IMPLIED, INCLUDING (WITHOUT LIMITATION) WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT, AND
+// TITLE.
+
+using Kiparis.EventBus.Abstractions;
+using Kiparis.EventBus.Dapr;
+using Microsoft.Extensions.Hosting;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Provides extension methods for adding the Dapr event bus to the application.
+/// </summary>
+public static class DaprEventBusExtensions
+{
+    /// <summary>
+    /// Adds the Dapr event bus to the application.
+    /// </summary>
+    /// <param name="builder">The host builder.</param>
+    /// <returns>The host builder.</returns>
+    public static IHostApplicationBuilder AddDaprEventBus(this IHostApplicationBuilder builder)
+    {
+        builder.Services.AddSingleton<IEventBus, DaprEventBus>();
+        
+        return builder;
+    }
+}
---
+++ src/Kiparis.EventBus/Events/IntegrationEvent.cs
@@ -0,0 +1,37 @@
+﻿// SPDX-License-Identifier: BUSL-1.1
+//
+// Copyright (c) 2024 Parusnik-Belgorod, LLC. All rights reserved.
+// Use of this software is govered by the Business Source License included
+// in the LICENSE file of this repository and at www.mariadb.com/bsl11.
+//
+// ANY USE OF THE LICENSED WORK IN VIOLATION OF THIS LICENSE WILL AUTOMATICALLY
+// TERMINATE YOUR RIGHTS UNDER THIS LICENSE FOR THE CURRENT AND ALL OTHER
+// VERSIONS OF THE LICENSED WORK.
+//
+// THIS LICENSE DOES NOT GRANT YOU ANY RIGHT IN ANY TRADEMARK OR LOGO OF
+// LICENSOR OR ITS AFFILIATES (PROVIDED THAT YOU MAY USE A TRADEMARK OR LOGO OF
+// LICENSOR AS EXPRESSLY REQUIRED BY THIS LICENSE).
+//
+// TO THE EXTENT PERMITTED BY APPLICABLE LAW, THE LICENSED WORK IS PROVIDED ON
+// AN “AS IS” BASIS. LICENSOR HEREBY DISCLAIMS ALL WARRANTIES AND CONDITIONS,
+// EXPRESS OR IMPLIED, INCLUDING (WITHOUT LIMITATION) WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT, AND
+// TITLE.
+
+namespace Kiparis.EventBus.Events;
+
+/// <summary>
+/// Represents an event that is published to the event bus.
+/// </summary>
+public record IntegrationEvent
+{
+    /// <summary>
+    /// The unique identifier of the event.
+    /// </summary>
+    public Guid Id { get; private init; } = Guid.NewGuid();
+
+    /// <summary>
+    /// The date and time the event was created.
+    /// </summary>
+    public DateTime CreationDate { get; private init; } = DateTime.UtcNow;
+}
